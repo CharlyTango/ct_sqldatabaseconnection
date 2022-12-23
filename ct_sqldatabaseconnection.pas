fb15d2827eab872497a1059599e41216c0f8d773
CharlyTango <charlytango@gmx.at>
2022-12-23 19:58:50 +0100

    Add files via upload

---
+++ ct_sqldatabaseconnection.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ct_sqldatabaseconnection;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  fcredentials, udmsqldb, uguessfile;
+
+implementation
+
+end.
